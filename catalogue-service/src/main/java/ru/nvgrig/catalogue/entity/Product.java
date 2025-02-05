ba4f6185d52927183e16aa298cd8801419e5cf5a
Nikita Grigoryev <nvgrigoryev@gmail.com>
2025-02-05 11:28:33 +0400

    hql named query

--- catalogue-service/src/main/java/ru/nvgrig/catalogue/entity/Product.java
+++ catalogue-service/src/main/java/ru/nvgrig/catalogue/entity/Product.java
@@ -12,6 +12,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(schema = "catalogue", name = "t_product")
+@NamedQueries(
+        @NamedQuery(
+                name = "Product.findAllByTitleLikeIgnoringCase",
+                query = "select p from Product p where p.title ilike :filter"
+        )
+)
 public class Product {
 
     @Id
--- catalogue-service/src/main/java/ru/nvgrig/catalogue/repository/ProductRepository.java
+++ catalogue-service/src/main/java/ru/nvgrig/catalogue/repository/ProductRepository.java
@@ -1,10 +1,13 @@
 package ru.nvgrig.catalogue.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import ru.nvgrig.catalogue.entity.Product;
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
-    Iterable<Product> findAllByTitleLikeIgnoreCase(String filter);
+    @Query(name = "Product.findAllByTitleLikeIgnoringCase")
+    Iterable<Product> findAllByTitleLikeIgnoreCase(@Param("filter") String filter);
     
 }
