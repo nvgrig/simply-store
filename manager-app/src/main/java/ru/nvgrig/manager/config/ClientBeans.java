595dcdbbc565fb25cd75984d7e3484e6bcc1df98
Nikita Grigoryev <nvgrigoryev@gmail.com>
2025-02-07 17:10:46 +0400

    basic authorization

--- manager-app/src/main/java/ru/nvgrig/manager/config/ClientBeans.java
+++ manager-app/src/main/java/ru/nvgrig/manager/config/ClientBeans.java
@@ -3,6 +3,7 @@ package ru.nvgrig.manager.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestClient;
 import ru.nvgrig.manager.client.RestClientProductsRestClient;
 
@@ -10,9 +11,13 @@ import ru.nvgrig.manager.client.RestClientProductsRestClient;
 public class ClientBeans {
 
     @Bean
-    public RestClientProductsRestClient productsRestClient(@Value("${store.services.catalogue.uri:http//localhost:8081}") String catalogueBaseUri) {
+    public RestClientProductsRestClient productsRestClient(
+            @Value("${store.services.catalogue.uri:http//localhost:8081}") String catalogueBaseUri,
+            @Value("${store.services.catalogue.username:}") String catalogueUsername,
+            @Value("${store.services.catalogue.password:}") String cataloguePassword) {
         return new RestClientProductsRestClient(RestClient.builder()
                 .baseUrl(catalogueBaseUri)
+                .requestInterceptor(new BasicAuthenticationInterceptor(catalogueUsername, cataloguePassword))
                 .build());
     }
 
